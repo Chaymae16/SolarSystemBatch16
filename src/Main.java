12244552fbaebb03e21df56d4ffaf0b743797175
Chaymae16 <chaymaeboufnichel39@gmail.com>
2023-07-08 14:10:05 -0400

    i have added sun but it has some errors to be resolved

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,12 @@ public class Main {
         Omanax.Sun="1";
         Omanax.Planets="2";
         Omanax.Star="4";
+
+        FeatureSun AdamX=new FeatureSun();
+        AdamX.name="poya";
+        AdamX.temp=12344231 celcius;
+        AdamX.size="32942Km";
+
+
     }
 }
