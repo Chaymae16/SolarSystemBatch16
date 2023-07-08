25de0cefca851bdb693ae679eb13875944f7018f
Chaymae16 <chaymaeboufnichel39@gmail.com>
2023-07-08 12:33:39 -0400

    The basic solar system is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystemBasic Omanax=new SolarSystemBasic();
+        Omanax.Moon="2";
+        Omanax.Sun="1";
+        Omanax.Planets="2";
+        Omanax.Star="4";
+    }
+}
